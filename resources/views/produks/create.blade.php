2b1e1d587264252a225164269a1e2354ad67d01c
Rafi <rafiudinjafar@gmail.com>
2024-02-08 06:05:13 +0800

    first commit

---
+++ resources/views/produks/create.blade.php
@@ -0,0 +1,55 @@
+@extends('menus.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>tambah barang proyek baru</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('produks.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+     
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+     
+<form action="{{ route('produks.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama Barang:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="nama">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Stok:</strong>
+                <input class="form-control" name="kategori" placeholder="kategori"></input>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Harga:</strong>
+                <textarea class="form-control" name="harga_jual" placeholder="harga"></textarea>
+            </div>
+        </div>
+        <br>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+     
+</form>
+@endsection
